2f40f20f7365b31597687774300a7f2b585c4c17
Isak Horvath <isakhorvath@gmail.com>
2025-07-19 18:50:27 +0200

    ✨ GH-BEM: Add HTML Node structure
    Add HTML Node structure and related structures needed.
    
    This commit was written using the Jakel Editor.

--- parser/html-parser.c
+++ parser/html-parser.c
@@ -1,6 +1,36 @@
 #include <stdbool.h>
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
+
+typedef enum {
+	DOCUMENT,
+	ELEMENT,
+	TEXT,
+	COMMENT
+} HTMLNodeType;
+
+typedef struct HTMLAttribute {
+	char* name;
+	char* value;
+
+	struct HTMLAttribute* next;
+} HTMLAttribute;
+
+typedef struct HTMLNode {
+	HTMLNodeType type;
+
+	char* name;
+	char* content;
+
+	HTMLAttribute* attributes;
+
+	struct HTMLNode** children;
+	size_t childCount;
+	size_t childCapacity;
+
+	struct HTMLNode* parent;
+} HTMLNode;
 
 void parser(char* inputString) {
 	int inputStringLength = strlen(inputString);
