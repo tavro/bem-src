4e4a29222f472d19256547e16ee88e94e10f6469
Isak Horvath <isakhorvath@gmail.com>
2025-07-22 22:09:16 +0200

    ✨ GH-BEM: Set up SDL3
    This commit was written using the Jakel text editor.

--- renderer/renderer.c
+++ renderer/renderer.c
@@ -1,3 +1,48 @@
-int main() {
-    return 0;
+#define SDL_MAIN_USE_CALLBACKS 1 // Use callbacks instead of main()
+
+#include <SDL3/SDL.h>
+#include <SDL3/SDL_main.h>
+
+static SDL_Window *window = NULL;
+static SDL_Renderer *renderer = NULL;
+
+SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[]) { // This function runs once on start 
+    SDL_SetAppMetadata("Example Renderer Clear", "1.0", "com.example.renderer-clear");
+
+    if (!SDL_Init(SDL_INIT_VIDEO)) {
+        SDL_Log("Couldn't initialize SDL: %s", SDL_GetError());
+        return SDL_APP_FAILURE;
+    }
+
+    if (!SDL_CreateWindowAndRenderer("Test SDL3", 640, 480, 0, &window, &renderer)) {
+        SDL_Log("Couldn't create window/renderer: %s", SDL_GetError());
+        return SDL_APP_FAILURE;
+    }
+
+    return SDL_APP_CONTINUE;
+}
+
+SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event) { // This function runs when an event occurs (mouse input, key presses, etc...)
+    if (event->type == SDL_EVENT_QUIT) {
+        return SDL_APP_SUCCESS;
+    }
+    return SDL_APP_CONTINUE;
+}
+
+SDL_AppResult SDL_AppIterate(void *appstate) { // This function runs once per frame.
+    const double now = ((double)SDL_GetTicks()) / 1000.0;
+
+    const float red = (float) (0.5 + 0.5 * SDL_sin(now));
+    const float green = (float) (0.5 + 0.5 * SDL_sin(now + SDL_PI_D * 2 / 3));
+    const float blue = (float) (0.5 + 0.5 * SDL_sin(now + SDL_PI_D * 4 / 3));
+    
+    SDL_SetRenderDrawColorFloat(renderer, red, green, blue, SDL_ALPHA_OPAQUE_FLOAT);
+    SDL_RenderClear(renderer); // Clear window using draw color
+    SDL_RenderPresent(renderer);
+
+    return SDL_APP_CONTINUE;
+}
+
+void SDL_AppQuit(void *appstate, SDL_AppResult result) { // This function runs once on app quit.
+    // SDL will clean up the window/renderer for us.
 }
