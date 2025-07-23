f662899e7d6bfca948fd2a8957b273e13d411513
Isak Horvath <isakhorvath@gmail.com>
2025-07-23 21:15:36 +0200

    ✨ GH-BEM: Render font with SDL3_TTF instead
    Use SDL3_TTF for rendering text instead.
    
    This commit was written using the Jakel text editor.

--- renderer/renderer.c
+++ renderer/renderer.c
@@ -3,17 +3,23 @@
 #include "../parser/html-parser.h"
 #include "../parser/css-parser.h"
 #include "../utils/fetch.h"
+#include "./resources/tiny-ttf.h"
 
 #include <SDL3/SDL.h>
 #include <SDL3/SDL_main.h>
+#include <SDL3_ttf/SDL_ttf.h>
+
 #include <stdio.h>
 
 static SDL_Window *window = NULL;
 static SDL_Renderer *renderer = NULL;
+
+static TTF_Font *font = NULL;
+
 HTMLNode *DOM = NULL;
 
 SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[]) { // This function runs once on start 
-    SDL_SetAppMetadata("Example Renderer Clear", "1.0", "com.example.renderer-clear");
+    SDL_SetAppMetadata("bem", "1.0", "com.tavro.bem");
 
     if (!SDL_Init(SDL_INIT_VIDEO)) {
         SDL_Log("Couldn't initialize SDL: %s", SDL_GetError());
@@ -25,7 +31,19 @@ SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[]) { // This fun
         return SDL_APP_FAILURE;
     }
 
-    const char *url = "https://www.tuhs.org/";
+    if (!TTF_Init()) {
+        SDL_Log("Couldn't initialise SDL_ttf: %s\n", SDL_GetError());
+        return SDL_APP_FAILURE;
+    }
+
+    // font = TTF_OpenFontIO(SDL_IOFromConstMem(tiny_ttf, tiny_ttf_len), true, 16);
+    font = TTF_OpenFont("./resources/DejaVuSansMonoForConso1as.ttf", 16);
+    if (!font) {
+        SDL_Log("Couldn't open font: %s\n", SDL_GetError());
+        return SDL_APP_FAILURE;
+    }
+
+    const char *url = "https://www.example.com"; // tuhs.org/";
 	char *htmlInput = NULL;
 	char *cssInput = NULL;
 
@@ -52,8 +70,24 @@ static int yPos = 0;
 void renderNode(HTMLNode* node) {
 	switch(node->type) {
 		case TEXT:
-            SDL_RenderDebugText(renderer, 0, yPos, node->content);
-            yPos += 7;
+            SDL_Color textColor = {255, 255, 255, 255};
+
+            size_t textLen = strlen(node->content);
+
+            SDL_Surface* textSurface = TTF_RenderText_Solid(font, node->content, textLen, textColor);
+			SDL_Texture* textTexture = SDL_CreateTextureFromSurface(renderer, textSurface);
+
+            SDL_FRect dstRect = {
+				.x = 0.0f,
+				.y = (float)(yPos),
+				.w = (float)textSurface->w,
+				.h = (float)textSurface->h
+			};
+			SDL_RenderTexture(renderer, textTexture, NULL, &dstRect);
+			yPos += textSurface->h;
+
+            SDL_DestroySurface(textSurface);
+			SDL_DestroyTexture(textTexture);
 			break;
 		default:
 			break;
