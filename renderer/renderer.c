9b48060b7664c8be61dfdf05332d55f65070fa70
Isak Horvath <isakhorvath@gmail.com>
2025-07-23 19:02:12 +0200

    ✨ GH-BEM: Render text content of DOM
    For now using SDL built-in function to render debug text. I plan to use SDL_ttf 3.0 moving forward.
    
    This commit was written using the Jakel text editor.

--- renderer/renderer.c
+++ renderer/renderer.c
@@ -1,10 +1,16 @@
 #define SDL_MAIN_USE_CALLBACKS 1 // Use callbacks instead of main()
 
+#include "../parser/html-parser.h"
+#include "../parser/css-parser.h"
+#include "../utils/fetch.h"
+
 #include <SDL3/SDL.h>
 #include <SDL3/SDL_main.h>
+#include <stdio.h>
 
 static SDL_Window *window = NULL;
 static SDL_Renderer *renderer = NULL;
+HTMLNode *DOM = NULL;
 
 SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[]) { // This function runs once on start 
     SDL_SetAppMetadata("Example Renderer Clear", "1.0", "com.example.renderer-clear");
@@ -14,10 +20,21 @@ SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[]) { // This fun
         return SDL_APP_FAILURE;
     }
 
-    if (!SDL_CreateWindowAndRenderer("Test SDL3", 640, 480, 0, &window, &renderer)) {
+    if (!SDL_CreateWindowAndRenderer("bem", 640, 480, 0, &window, &renderer)) {
         SDL_Log("Couldn't create window/renderer: %s", SDL_GetError());
         return SDL_APP_FAILURE;
     }
+
+    const char *url = "https://www.tuhs.org/";
+	char *htmlInput = NULL;
+	char *cssInput = NULL;
+
+	int res = fetchHTMLAndCSS(url, &htmlInput, &cssInput);
+	if (res == 0) {
+		extractMetaData(&htmlInput);
+	}
+
+	DOM = parser(htmlInput);
 
     return SDL_APP_CONTINUE;
 }
@@ -29,15 +46,35 @@ SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event) { // This function 
     return SDL_APP_CONTINUE;
 }
 
-SDL_AppResult SDL_AppIterate(void *appstate) { // This function runs once per frame.
-    const double now = ((double)SDL_GetTicks()) / 1000.0;
 
-    const float red = (float) (0.5 + 0.5 * SDL_sin(now));
-    const float green = (float) (0.5 + 0.5 * SDL_sin(now + SDL_PI_D * 2 / 3));
-    const float blue = (float) (0.5 + 0.5 * SDL_sin(now + SDL_PI_D * 4 / 3));
-    
-    SDL_SetRenderDrawColorFloat(renderer, red, green, blue, SDL_ALPHA_OPAQUE_FLOAT);
+// TODO: Add browser default CSSOM
+static int yPos = 0;
+void renderNode(HTMLNode* node) {
+	switch(node->type) {
+		case TEXT:
+            SDL_RenderDebugText(renderer, 0, yPos, node->content);
+            yPos += 7;
+			break;
+		default:
+			break;
+	}
+
+	for (size_t i = 0; i < node->childCount; i++) {
+		renderNode(node->children[i]);
+	}
+}
+
+SDL_AppResult SDL_AppIterate(void *appstate) { // This function runs once per frame.
+    // const double now = ((double)SDL_GetTicks()) / 1000.0;
+
+    SDL_SetRenderDrawColor(renderer, 0, 0, 0, SDL_ALPHA_OPAQUE);
     SDL_RenderClear(renderer); // Clear window using draw color
+
+    SDL_SetRenderDrawColor(renderer, 255, 255, 255, SDL_ALPHA_OPAQUE);
+
+    yPos = 0;
+    renderNode(DOM);
+
     SDL_RenderPresent(renderer);
 
     return SDL_APP_CONTINUE;
